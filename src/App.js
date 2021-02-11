c95748296501cd7ac574ebf7651310056bd6e0bb
ngoba <ngoba9x@gmail.com>
2021-02-11 22:02:04 +0700

    nothing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense, useEffect } from "react";
 import jwt from "jsonwebtoken";
-import { BrowserRouter as Router, Route, Switch ,Redirect} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
@@ -12,43 +12,42 @@ import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer";
 import Home from "./components/Pages/Home"
 import ChatApp from "./components/Socketio/ChatApp";
-import {GoHomeIfLogged} from "./router"
+import { GoHomeIfLogged } from "./router"
 
 
 
 export default function App() {
-const token = localStorage.getItem("lite-friend");
+  const token = localStorage.getItem("lite-friend");
 
-if (token) {
-  const currentTime = Date.now() / 1000;
-  const decoded = jwt.decode(token, { complete: true });
-  if (currentTime > decoded.exp) {
-    localStorage.removeItem("lite-friend");
-    
-  } else {
-    setAuthHeader(token)
+  if (token) {
+    const currentTime = Date.now() / 1000;
+    const decoded = jwt.decode(token, { complete: true });
+    if (currentTime > decoded.exp) {
+      localStorage.removeItem("lite-friend");
+
+    } else {
+      setAuthHeader(token)
+    }
   }
-}
   return (
     <Router>
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-        <Route exact path="/forgot-password" component={ForgotPassword} />  
-       < Route exact path="/profile/:userId" component={ProfileRender}/>
-      
-        {token ? <Home/>:  <Login />}
+        <Route exact path="/forgot-password" component={ForgotPassword} />
+        < Route exact path="/profile/:userId" component={ProfileRender} />
+        {token ? <Home /> : <Login />}
       </Switch>
     </Router>
-    )
-    
+  )
+
 
 }
 
 
 // export default function App() {
 //   const isLoading  = useRecoilValueLoadable(isMeRecoil).state === "loading" ? false :true;
- 
+
 //   return (
 //     isLoading &&(
 //     <Router>
@@ -71,6 +70,6 @@ if (token) {
 //       </Switch>
 //     </Router>
 //     )
-    
+
 //   );
 // }
